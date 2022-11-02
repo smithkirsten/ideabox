1a29f1c53aad448d07606c94f83cfa78d8c54e03
John Ammon <ammonj78@gmail.com>
2022-11-02 15:20:24 -0600

    Adds sudo code for data modle

--- main.js
+++ main.js
@@ -4,9 +4,30 @@ var Idea = require('../ideabox/Idea');
 
 var savedIdeas = [];
 
-
+//Iteration 2
+// disable save button until both input fields have content
+    // css sudo class disabled can work like hidden 
+        // might have to change cursser property sepretly
+        
 // we will need a save function to push new options in to the new saved array
+// prevent default on save button
 
-// we will need a delete function that uses event delligation
+    //gather user input instantiate into class 
+    //push new object into saved array
+    //clear input fields
 
-// we will need a cunction to create a new card that will be pushed into the saved grid
+// we will need a function to create a new card that will be pushed into the saved grid
+    //interpolate values from new object instance into inner HTML for .idea-card-grid
+
+// Iteration 3
+
+// we will need a delete function that uses event delegation
+    // put listener on grid
+    // if(event.target.classList.contains(delete button))
+        // event.target.closest(.ideaCard).remove()
+    // splice object instance from savedIdeas array 
+
+
+
+// faverit function
+
